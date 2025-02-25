2ef8dd3abcd1513ff667dcbc93e9ab9afda8b480
hoinhoin <follow3448@naver.com>
2025-02-25 23:38:29 +0900

    책등록 화면 생성

--- app/src/main/java/com/example/munjangzip/MainAppComposable.kt
+++ app/src/main/java/com/example/munjangzip/MainAppComposable.kt
@@ -8,14 +8,16 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.munjangzip.feature.auth.signin.SignInScreen
+import com.example.munjangzip.feature.booklist.BookListScreen
 import com.example.munjangzip.feature.category.CategoryScreen
+import com.example.munjangzip.feature.savebook.TakePhotoPage
 
 @Composable
 fun MainApp() {
     Surface(modifier = Modifier.fillMaxSize()) {
         val navController = rememberNavController()
 
-        NavHost(navController = navController, startDestination = "category") {
+        NavHost(navController = navController, startDestination = "takephoto") {
             composable(route = "login") {
                 SignInScreen(navController = navController)
             }
@@ -28,6 +30,13 @@ fun MainApp() {
                 CategoryScreen(navController = navController)
             }
 
+            composable(route = "booklist") {
+                BookListScreen(navController = navController)
+            }
+
+            composable(route = "takephoto") {
+                TakePhotoPage(navController = navController)
+            }
         }
 
     }
