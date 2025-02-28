e0c3cebc83a51188f6660ff701177916c8f1651a
shu030929 <shu030929@gmail.com>
2025-03-01 04:03:22 +0900

    UI: ui 수정

--- app/src/main/java/com/example/munjangzip/ui/theme/Color.kt
+++ app/src/main/java/com/example/munjangzip/ui/theme/Color.kt
@@ -10,8 +10,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-
-val PeachYellow = Color(0xFFFFE7AE)
-val BrightYellow = Color(0xfffff2d3)
+val BrightYellow = Color(0xFFFFF2D3)
 val Ivory = Color(0xfffffef3)
 val Gray10 = Color(0xff888888)
