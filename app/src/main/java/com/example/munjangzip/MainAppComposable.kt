27b19b8a763a71b5859cc0a6cdd6ea034b955487
shu030929 <shu030929@gmail.com>
2025-03-08 18:27:50 +0900

    feat: 메모생성 top바 추가

--- app/src/main/java/com/example/munjangzip/MainAppComposable.kt
+++ app/src/main/java/com/example/munjangzip/MainAppComposable.kt
@@ -26,7 +26,7 @@ fun MainApp() {
     Surface(modifier = Modifier.fillMaxSize()) {
         val navController = rememberNavController()
 
-        NavHost(navController = navController, startDestination = "register") {
+        NavHost(navController = navController, startDestination = "category") {
 
             composable(route = "login") {
                 SignInScreen(navController = navController)
