3708546d763e7a5cba716fd8b4450dbdf032c690
hoinhoin <follow3448@naver.com>
2025-03-14 20:02:18 +0900

    minor update

--- app/src/main/java/com/example/munjangzip/feature/booklist/BookListGrid.kt
+++ app/src/main/java/com/example/munjangzip/feature/booklist/BookListGrid.kt
@@ -27,101 +27,178 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.example.munjangzip.R
+//@Composable
+//fun BookListGrid(navController: NavController, viewModel: BookListViewModel, categoryId: Int) {
+//
+//
+//    // ✅ 서버에서 책 목록 가져오기
+//    LaunchedEffect(categoryId) {
+//        viewModel.fetchBookList(categoryId = categoryId) // 원하는 카테고리 ID 설정
+//    }
+//
+//    val bookListState by viewModel.bookListState.collectAsState()
+//    // ✅ 서버에서 가져온 책 목록 사용
+//    val books = bookListState.result.books
+//
+//// ✅ books가 비어 있을 경우 예외 방지
+//    if (books.isEmpty()) {
+//        LazyVerticalGrid(
+//            columns = GridCells.Fixed(3),
+//            contentPadding = PaddingValues(
+//                start = 12.dp,
+//                top = 16.dp,
+//                end = 12.dp,
+//                bottom = 16.dp
+//            ),
+//            content = {
+//                items(1){
+//                    Card(
+//                        modifier = Modifier
+//                            .padding(8.dp)
+//                            .size(width = 40.dp, height = 160.dp)
+//
+//                    ){
+//
+//                        Image(
+//                            painter = painterResource(R.drawable.addbook),
+//                            contentDescription = "책 추가 버튼",
+//                            contentScale = ContentScale.Crop,
+//                            modifier = Modifier.clickable {
+//                                //navController.navigate("takephoto")
+//                                navController.navigate("takephoto/$categoryId")
+//                            }
+//                        )
+//                    }
+//                }
+//
+//            })
+//
+//    }
+//    else {
+//        LazyVerticalGrid(
+//            columns = GridCells.Fixed(3),
+//            contentPadding = PaddingValues(
+//                start = 12.dp,
+//                top = 16.dp,
+//                end = 12.dp,
+//                bottom = 16.dp
+//            ),
+//            content = {
+//                items(books.size + 1) { index ->
+//                    Card(
+//                        modifier = Modifier
+//                            .padding(8.dp)
+//                            .size(width = 40.dp, height = 160.dp)
+//                            .clickable {
+//                                if (index < books.size) {
+//                                    //navController.navigate("bookDetail/${index}")
+//                                    navController.navigate("bookDetail/${books[index].bookId}")
+//                                }
+//                            }
+//                    ) {
+//                        if (index == books.size) {
+//                            Image(
+//                                painter = painterResource(R.drawable.addbook),
+//                                contentDescription = "책 추가 버튼",
+//                                contentScale = ContentScale.Crop,
+//                                modifier = Modifier.clickable {
+//                                    //navController.navigate("takephoto")
+//                                    navController.navigate("takephoto/$categoryId")
+//
+//                                }
+//                            )
+//                        } else {
+//
+//                            // ✅ 서버에서 가져온 책 표지 이미지 표시 (Coil 사용)
+//                            AsyncImage(
+//                                model = books[index].bookImage,
+//                                contentDescription = "Book Image ${books[index].title}",
+//                                contentScale = ContentScale.FillBounds,
+//                                modifier = Modifier.fillMaxSize()
+//                            )
+//                        }
+//                    }
+//                }
+//            }
+//        )
+//    }
+//
+//
+//}
 @Composable
 fun BookListGrid(navController: NavController, viewModel: BookListViewModel, categoryId: Int) {
 
-
-    // ✅ 서버에서 책 목록 가져오기
     LaunchedEffect(categoryId) {
-        viewModel.fetchBookList(categoryId = categoryId) // 원하는 카테고리 ID 설정
+        viewModel.fetchBookList(categoryId)
     }
 
     val bookListState by viewModel.bookListState.collectAsState()
-    // ✅ 서버에서 가져온 책 목록 사용
-    val books = bookListState.result.books
+    val books = bookListState?.result?.books ?: emptyList()
 
-// ✅ books가 비어 있을 경우 예외 방지
-    if (books.isEmpty()) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
-            contentPadding = PaddingValues(
-                start = 12.dp,
-                top = 16.dp,
-                end = 12.dp,
-                bottom = 16.dp
-            ),
-            content = {
-                items(1){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        contentPadding = PaddingValues(
+            start = 12.dp,
+            top = 16.dp,
+            end = 12.dp,
+            bottom = 16.dp
+        ),
+        content = {
+            if (books.isEmpty()) {
+                // 책이 없을 경우 추가 버튼만 표시
+                item {
                     Card(
                         modifier = Modifier
                             .padding(8.dp)
-                            .size(width = 40.dp, height = 160.dp)
-
-                    ){
-
+                            .size(width = 100.dp, height = 160.dp)
+                    ) {
                         Image(
                             painter = painterResource(R.drawable.addbook),
                             contentDescription = "책 추가 버튼",
                             contentScale = ContentScale.Crop,
                             modifier = Modifier.clickable {
-                                //navController.navigate("takephoto")
                                 navController.navigate("takephoto/$categoryId")
                             }
                         )
                     }
                 }
-
-            })
-
-    }
-    else {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
-            contentPadding = PaddingValues(
-                start = 12.dp,
-                top = 16.dp,
-                end = 12.dp,
-                bottom = 16.dp
-            ),
-            content = {
-                items(books.size + 1) { index ->
+            } else {
+                // 책 목록 표시
+                items(books.size) { index ->
                     Card(
                         modifier = Modifier
                             .padding(8.dp)
-                            .size(width = 40.dp, height = 160.dp)
+                            .size(width = 100.dp, height = 160.dp)
                             .clickable {
-                                if (index < books.size) {
-                                    //navController.navigate("bookDetail/${index}")
-                                    navController.navigate("bookDetail/${books[index].bookId}")
-                                }
+                                navController.navigate("bookDetail/${books[index].bookId}")
                             }
                     ) {
-                        if (index == books.size) {
-                            Image(
-                                painter = painterResource(R.drawable.addbook),
-                                contentDescription = "책 추가 버튼",
-                                contentScale = ContentScale.Crop,
-                                modifier = Modifier.clickable {
-                                    //navController.navigate("takephoto")
-                                    navController.navigate("takephoto/$categoryId")
-
-                                }
-                            )
-                        } else {
-
-                            // ✅ 서버에서 가져온 책 표지 이미지 표시 (Coil 사용)
-                            AsyncImage(
-                                model = books[index].bookImage,
-                                contentDescription = "Book Image ${books[index].title}",
-                                contentScale = ContentScale.FillBounds,
-                                modifier = Modifier.fillMaxSize()
-                            )
-                        }
+                        AsyncImage(
+                            model = books[index].bookImage,
+                            contentDescription = "Book Image ${books[index].title}",
+                            contentScale = ContentScale.FillBounds,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                }
+                // 마지막에 책 추가 버튼 표시
+                item {
+                    Card(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .size(width = 100.dp, height = 160.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(addbook),
+                            contentDescription = "책 추가 버튼",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.clickable {
+                                navController.navigate("takephoto/$categoryId")
+                            }
+                        )
                     }
                 }
             }
-        )
-    }
-
-
+        }
+    )
 }
